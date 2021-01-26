9ed1f6ce8e424c2213135e2ae05063616b7af873
Fabio <fabiomarsiaj@gmail.com>
2021-01-26 14:46:57 -0300

    Place building with mouse click

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform pfWoodHarvester;
+
+    private Camera _mainCamera;
+
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
