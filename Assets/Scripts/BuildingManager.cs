d2807bb323ca8e45a995dc6a63977cd483c1a706
Fabio <fabiomarsiaj@gmail.com>
2021-01-26 15:32:12 -0300

    ScriptableObjects for buildingTypes and buildingTypeList

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class BuildingManager : MonoBehaviour
 {
 
-    [SerializeField]
-    private Transform pfWoodHarvester;
 
+    private BuildingTypeSO _buildingType;
+    private BuildingTypeListSO _buildingTypeList;
     private Camera _mainCamera;
 
     void Start()
     {
         _mainCamera = Camera.main;
+
+        _buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        _buildingType = _buildingTypeList.list[0];
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+            Instantiate(_buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _buildingType = _buildingTypeList.list[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            _buildingType = _buildingTypeList.list[1];
         }
     }
 
